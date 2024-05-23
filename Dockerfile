8b296146a63d0dae181b4451cb2a99eed665baec
Valentino Scevaroli <valentino.scevaroli@mia-platform.eu>
2024-05-23 10:03:59 +0200

    Update gitlab e dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM nginx:1.17.2-alpine as build
 
-LABEL name="mia_template_service_name_placeholder" \
+LABEL name="vite_typescript_react_app_template" \
       description="%CUSTOM_PLUGIN_SERVICE_DESCRIPTION%" \
       eu.mia-platform.url="https://www.mia-platform.eu" \
       eu.mia-platform.version="0.1.0"
@@ -9,7 +9,7 @@ COPY nginx /etc/nginx
 
 RUN touch ./off \
   && chmod o+rw ./off \
-  && echo "mia_template_service_name_placeholder: $COMMIT_SHA" >> /etc/nginx/commit.sha
+  && echo "vite_typescript_react_app_template: $COMMIT_SHA" >> /etc/nginx/commit.sha
 
 WORKDIR /usr/static
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,32 +3,42 @@ import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react-swc'
 import { configDefaults } from 'vitest/config'
 import {resolve} from 'path'
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './',
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    mockReset: true,
-    logHeapUsage: true,
-    reporters: [
-      'default',
+export default defineConfig(({ command, mode }) => {
+
+  return {
+    base: './',
+    plugins: [
+      react(),
+      checker({
+        typescript: true,
+      }),
     ],
-    setupFiles: [
-      resolve(__dirname, 'src/setupTests.jsx')
-    ],
-    coverage: {
-      exclude: [
-        ...configDefaults.exclude,
-        'build', 'mock-server', 'src/__mocks__', '.eslintrc.cjs'] 
-    }
-  },
-  server: {
-    port: 3000
-  },
-  build: {
-    outDir: 'build'
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      mockReset: true,
+      logHeapUsage: true,
+      reporters: [
+        'default',
+      ],
+      setupFiles: [
+        resolve(__dirname, 'src/setupTests.jsx')
+      ],
+      coverage: {
+        exclude: [
+          ...configDefaults.exclude,
+          'build', 'mock-server', 'src/__mocks__', '.eslintrc.cjs']
+      }
+    },
+    server: {
+      port: 3000,
+    },
+    build: {
+      outDir: 'build'
+    },
+  
   }
 })
